1b62b1e82ce57ec1a263c9f00d97f9197f0ebf75
eniacce <mesutaygn@gmail.com>
2026-06-22 03:43:48 +0300

    test added

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/saldir2')
+def hello_world():  # put application's code here
+    return 'Hello World saldir2!'
+
 
 if __name__ == '__main__':
     app.run()
