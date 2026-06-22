d5f8e3b89613ed3872fe6c7b942b2d826f88465d
eniacce <mesutaygn@gmail.com>
2026-06-22 04:09:11 +0300

    test added

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def hello_world():  # put application's code here
 def hello_world2():  # put application's code here
     return 'Hello World saldir2!'
 
+@app.route('/saldir3')
+def hello_world3():  # put application's code here
+    return 'Hello World saldir3!'
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=8080)
