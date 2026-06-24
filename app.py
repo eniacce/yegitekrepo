ec2ecb4f5567c5c079f4202f28d0d54e5f179309
eniacce <mesutaygn@gmail.com>
2026-06-24 15:14:25 +0300

    test added

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    a=12
-    b=5;
 
-    return 'Hello World! {}' + a // b
+
+    return 'Hello World! test3'
 
 @app.route('/saldir2')
 def hello_world2():  # put application's code here
